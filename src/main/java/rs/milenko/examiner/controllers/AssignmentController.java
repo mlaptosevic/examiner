620ae62f4414cd771a04a149321042c235b7a768
Milenko <mlaptosevic@gmail.com>
2019-07-21 10:34:23 +0200

    Added swagger

--- src/main/java/rs/milenko/examiner/controllers/AssignmentController.java
+++ src/main/java/rs/milenko/examiner/controllers/AssignmentController.java
@@ -1,5 +1,9 @@
 package rs.milenko.examiner.controllers;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +16,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/v1/assignment")
+@Api("Set of endpoints used for Assignment")
 public class AssignmentController {
 
     private AssignmentService assignmentService;
@@ -22,18 +27,21 @@ public class AssignmentController {
         this.questionService = questionService;
     }
 
-    @RequestMapping("/all")
+    @GetMapping("/all")
+    @ApiOperation("Retrieving all assignment, if no assignment is available will return empty array")
     public List<Assignment> getAllAssignments() {
         return assignmentService.getAllAssignments();
     }
 
-    @RequestMapping("/{id}/text")
-    public String getAssignmentText(@PathVariable long id) {
+    @GetMapping("/{id}/text")
+    @ApiOperation("Retrieving text for specified assignment")
+    public String getAssignmentText(@ApiParam("Id of assignment") @PathVariable long id) {
         return assignmentService.getAssignmentText(id);
     }
 
-    @RequestMapping("/{id}/questions")
-    public List<Question> getQuestions(@PathVariable long id) {
+    @GetMapping("/{id}/questions")
+    @ApiOperation("Retrieving randomly created questions for specified assignment")
+    public List<Question> getQuestions(@ApiParam("Id of assignment") @PathVariable long id) {
         return questionService.getQuestionsForAssignment(id);
     }
 
